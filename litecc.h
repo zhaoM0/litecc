b6e37430c9691e6c6023a60acf19f2ed49427da7
zhaomo <koblodjuly@gmail.com>
2025-01-01 22:15:57 +0800

    Annotate AST nodes with types to make pointer arithmetic work

--- litecc.h
+++ litecc.h
@@ -13,6 +13,8 @@
   #define debug(fmt, ...) 
 #endif
 
+typedef struct Type Type;
+
 //
 // tokenize.c
 //
@@ -67,8 +69,11 @@ struct VarList {
 };
 
 typedef enum {
-  ND_ADD,        // +
-  ND_SUB,        // -
+  ND_ADD,        // num + num
+  ND_PTR_ADD,    // ptr + num or num + ptr
+  ND_SUB,        // num - num
+  ND_PTR_SUB,    // ptr - num
+  ND_PTR_DIFF,   // ptr - ptr
   ND_MUL,        // *
   ND_DIV,        // /
   ND_EQ,         // ==
@@ -94,6 +99,7 @@ typedef struct Node Node;
 struct Node {
   NodeKind kind;  // Node kind
   Node* next;     // Next node
+  Type* ty;       // Type, e.g. int or pointer to int
   Token* tok;     // Representative token
 
   Node* lhs;      // Left-hand side
@@ -128,6 +134,20 @@ struct Function {
 };
 
 Function *program(void);
+
+//
+// typing.c
+//
+
+typedef enum { TY_INT, TY_PTR } TypeKind;
+
+struct Type {
+  TypeKind kind;
+  Type* base;
+};
+
+bool is_integer(Type *ty);
+void add_type(Node* node);
 
 //
 // codegen.c
