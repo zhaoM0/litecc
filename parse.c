80377d03d183e59d720d474f0785ed98eeb27d70
zhaomo <koblodjuly@gmail.com>
2025-01-19 21:32:39 +0800

    Add [] operator

--- parse.c
+++ parse.c
@@ -71,6 +71,7 @@ static Node* relational(void);
 static Node* add(void);
 static Node* mul(void);
 static Node* unary(void);
+static Node* postfix(void);
 static Node* primary(void);
 
 
@@ -374,7 +375,7 @@ static Node* mul(void) {
 }
 
 // unary = ("+" | "-" | "*" | "&")? unary
-//       | primary
+//       | postfix
 static Node* unary(void) {
   Token* tok = NULL;
   if (consume("+"))
@@ -385,7 +386,21 @@ static Node* unary(void) {
     return new_unary(ND_ADDR, unary(), tok);
   if (tok = consume("*"))
     return new_unary(ND_DEREF, unary(), tok);
-  return primary();
+  return postfix();
+}
+
+// postfix = primary ("[" expr "]")*
+static Node* postfix(void) {
+  Node* node = primary();
+  Token* tok;
+
+  while (tok = consume("[")) {
+    // x[y] is short for *(x+y)
+    Node* exp = new_add(node, expr(), tok);
+    expect("]");
+    node = new_unary(ND_DEREF, exp, tok);
+  }
+  return node;
 }
 
 // func-args = "(" (assign ("," assign)*)? ")"
